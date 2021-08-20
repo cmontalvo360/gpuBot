0a4650d7a442cefbf6f9284d677d777e8eaee838
Cesar Montalvo <cmontalvo360@yahoo.com>
2021-08-20 09:14:37 -0700

    Add files via upload
    nearly finished product

--- gpu_bot/gpuBot.py
+++ gpu_bot/gpuBot.py
@@ -1,18 +1,70 @@
+#!/usr/bin/env python3
+
 import time
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+
 
 driver = webdriver.Chrome()
-driver.get("https://www.bestbuy.com/site/lg-65-class-cx-series-oled-4k-uhd-smart-webos-tv/6401850.p?skuId=6401850")
+driver.implicitly_wait(10)
+driver.get("https://www.bestbuy.com/site/lenovo-chromebook-duet-10-1-tablet-128gb-with-keyboard-ice-blue-iron-gray/6401727.p?skuId=6401727")
 
+def main():
+    buyItem()
 
-element = driver.find_element_by_class_name("add-to-cart-button")
-element.click()
-time.sleep(3)
-gotocartbtn = driver.find_element_by_class_name("go-to-cart-button")
-gotocartbtn.click()
+def buyItem():
+    # finds add to cart button and clicks it
+    element = driver.find_element_by_class_name("add-to-cart-button")
+    element.click()
+    
+    # finds go to cart button and clicks it
+    gotocartbtn = driver.find_element_by_class_name("go-to-cart-button")
+    gotocartbtn.click()
 
-#deliverbtn = driver.find_element_by_class_name("availabilty__entry")
-#deliverbtn.click()
-time.sleep(3)
-checkoutbtn = driver.find_element_by_class_name("checkout-buttons__checkout")
-checkoutbtn.click()
+    #deliverbtn = driver.find_element_by_class_name("availabilty__entry")
+    #deliverbtn.click()
+
+    # finds go to cart button and clicks it
+    checkoutbtn = driver.find_element_by_class_name("checkout-buttons__checkout")
+    checkoutbtn.click()
+    
+    # continue checking out as guest 
+    guestbtn = driver.find_element_by_class_name("guest")
+    guestbtn.click()
+    
+    #fills in contact info
+    email = driver.find_element_by_id("user.emailAddress")
+    email.send_keys("cmontalvo360@yahoo.com")
+    phonenum = driver.find_element_by_id("user.phone")
+    phonenum.send_keys("2532927776")
+
+    # continue to payment screen
+    contbtn = driver.find_element_by_class_name("button--continue")
+    contbtn.click()
+
+    # fill in payment options
+    creditcard = driver.find_element_by_xpath("//*[@id='optimized-cc-card-number']")
+    creditcard.send_keys("2424887613140099")
+
+    #billing info
+    fname = driver.find_element_by_id("payment.billingAddress.firstName")
+    fname.send_keys("Joe")
+    lname = driver.find_element_by_id("payment.billingAddress.lastName")
+    lname.send_keys("Smoe")
+    addr_toggle = driver.find_element_by_class_name("autocomplete__toggle")
+    addr_toggle.click()
+    address = driver.find_element_by_id("payment.billingAddress.street")
+    address.send_keys("11223 72nd Avenue Ct E")
+    city = driver.find_element_by_id("payment.billingAddress.city")
+    city.send_keys("tacoma")
+    state = Select(driver.find_element_by_id("payment.billingAddress.state"))
+    state.select_by_value("WA")
+    zipcode = driver.find_element_by_id("payment.billingAddress.zipcode")
+    zipcode.send_keys("98444")
+
+    #place order
+    orderbtn = driver.find_element_by_class_name("payment__order-summary")
+    orderbtn.click()
+
+if __name__ == "__main__":
+    main()
